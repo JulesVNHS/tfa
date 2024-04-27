9220ee9e726f734eb514de248c6ea44c93e29bc3
JulesVNHS <98834604+JulesVNHS@users.noreply.github.com>
2024-04-27 21:46:29 +0200

    Menu et Home
    Création du menu sticky et de la section home

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,16 @@
-console.info('Hello world');
+"use strict";
+
+/*Sticky Menu*/
+let lastScrollTop = 0;
+const menu = document.querySelector('.menu');
+
+window.addEventListener('scroll', function() {
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  
+  if (scrollTop > lastScrollTop) {
+    menu.classList.add('menu--hidden');
+  } else {
+    menu.classList.remove('menu--hidden');
+  }
+  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+});
