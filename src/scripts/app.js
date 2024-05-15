f31941decd042be7f671aa232eb7b7655a625738
JulesVNHS <98834604+JulesVNHS@users.noreply.github.com>
2024-05-16 00:13:09 +0200

    Loading screen
    import de l'écran de chargement

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -477,3 +477,21 @@ document.addEventListener('DOMContentLoaded', function() {
     return array;
   }
 });
+
+/*Chargement*/
+$(document).ready(function() {
+	$('.skip').click(function() {
+		$('.overlay, body').addClass('loaded');
+	})
+
+	$(window).bind('load', function() {
+		$('.overlay, body').addClass('loaded');
+		setTimeout(function() {
+			$('.overlay').css({'display':'none'})
+		}, 2000)
+	});
+	
+	setTimeout(function() {
+		$('.overlay, body').addClass('loaded');
+	}, 60000);
+})
